b1957d316b1b0c17169ccf79088a87626b8178ac
unknown <fabianlopez928@gmail.com>
2022-11-15 15:48:41 -0500

    componente completo

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import BurgerButton from "./BurgerButton";
+
+function Navbar() {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    //cuando esta en tru lo pasa a false y veceversa
+    setClicked(!clicked);
+  };
+
+  return (
+    <>
+      <NavContainer>
+        <h2>
+          Navbar <span>Responsive</span>
+        </h2>
+        <div className={`links ${clicked ? "active" : ""}`}>
+          <a href="/">Home</a>
+          <a href="/">Shop</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Blog</a>
+        </div>
+        <div className="burger">
+          <BurgerButton clicked={clicked} handleClick={handleClick} />
+        </div>
+        <BgDiv className={`initial ${clicked?"active":""}`}></BgDiv>
+      </NavContainer>
+    </>
+  );
+}
+
+export default Navbar;
+
+const NavContainer = styled.nav`
+  h2 {
+    color: white;
+    font-weight: 400;
+    span {
+      font-weight: bold;
+    }
+  }
+  padding: 0.4rem;
+  background-color: #333;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    color: white;
+    text-decoration: none;
+    margin-right: 1rem;
+  }
+  .links {
+    position: absolute;
+    top: -700px;
+    left: -2000px;
+    right:0;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    transition:all .5s ease;
+    a {
+      color: white;
+      font-size: 2rem;
+      display: block;
+    }
+    @media (min-width: 768px) {
+      position: initial;
+      margin: 0;
+      a {
+        font-size: 1rem;
+        color: white;
+        display: inline;
+      }
+    }
+  }
+  .links.active {
+    width: 100%;
+    display: block;
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    top: 30%;
+    left: 0;
+    right: 0;
+    text-align: center;
+    a {
+      font-size: 2rem;
+      margin-top:1rem;
+      color: white;
+    }
+  }
+  .burger {
+    @media (min-width: 786px) {
+      display: none;
+    }
+  }
+`;
+const BgDiv = styled.div`
+  position:absolute;
+  background-color:#000;
+  top:-700px;
+  left:-2000px;
+  width:100%;
+  height:100%;
+  z-index:-1;
+  transition:all .6s ease;
+  &.active{
+    border-radius:0 0 80% 0 ;
+    top:0;
+    left:0;
+    width:100%;
+    height:100%;
+ 
+  }
+  
+`
